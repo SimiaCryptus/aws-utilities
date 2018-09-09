c2928c3a60e71a2dedbac1234d31e11b5d70ae79
Andrew Charneski <acharneski@gmail.com>
2018-09-09 09:21:41 -0700

    Misc Updates

--- src/main/java/com/simiacryptus/aws/S3Util.java
+++ src/main/java/com/simiacryptus/aws/S3Util.java
@@ -19,24 +19,32 @@
 
 package com.simiacryptus.aws;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.simiacryptus.util.Util;
+import com.simiacryptus.util.io.NotebookOutput;
 import com.simiacryptus.util.io.TeeInputStream;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * The type S3 util.
@@ -44,6 +52,39 @@ import java.util.function.Supplier;
 public class S3Util {
 
   private final static Logger logger = LoggerFactory.getLogger(S3Util.class);
+
+  public static Map<File, URL> upload(NotebookOutput log) {
+    return upload(log, AmazonS3ClientBuilder.standard().withRegion(Regions.US_WEST_2).build());
+  }
+
+  public static Map<File, URL> upload(NotebookOutput log, AmazonS3 s3) {
+    try {
+      log.write();
+      File root = log.getRoot();
+      logFiles(root);
+      URI archiveHome = log.getArchiveHome();
+      HashMap<File, URL> map = new HashMap<>();
+      if (null == archiveHome || (archiveHome.getScheme().startsWith("s3") && (null == archiveHome.getHost() || archiveHome.getHost().isEmpty()))) {
+        logger.info("No archive destination to publish to");
+        return map;
+      }
+      for (File file : root.listFiles()) {
+        map.putAll(S3Util.upload(s3, archiveHome, file));
+      }
+      return map;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void logFiles(File f) {
+    if (f.isDirectory()) {
+      for (File child : f.listFiles()) {
+        logFiles(child);
+      }
+    } else logger.info(String.format("File %s length %s", f.getAbsolutePath(), f.length()));
+  }
+
   public static Map<File, URL> upload(final AmazonS3 s3, final URI path, final File file) {
     try {
       logger.info(String.format("Uploading %s to %s", file, path));
@@ -51,7 +92,7 @@ public class S3Util {
       if (!file.exists()) throw new RuntimeException(file.toString());
       if (file.isFile()) {
         if (path.getScheme().startsWith("s3")) {
-          String key = path.getPath() + file.getName();
+          String key = path.resolve(file.getName()).getPath().replaceAll("//", "/").replaceAll("^/", "");
           s3.putObject(new PutObjectRequest(path.getHost(), key, file).withCannedAcl(CannedAccessControlList.PublicRead));
           map.put(file.getAbsoluteFile(), s3.getUrl(path.getHost(), key));
         } else {
@@ -63,8 +104,17 @@ public class S3Util {
           }
         }
       } else {
+        if (path.getScheme().startsWith("s3")) {
+          String prefix = path.resolve(file.getName() + "/").getPath();
+          List<S3ObjectSummary> preexistingFiles = s3.listObjects(new ListObjectsRequest().withBucketName(path.getHost()).withPrefix(prefix))
+              .getObjectSummaries().stream().collect(Collectors.toList());
+          for (S3ObjectSummary preexistingFile : preexistingFiles) {
+            logger.info(String.format("Preexisting File: '%s' + '%s'", prefix, preexistingFile.getKey()));
+            map.put(new File(file, preexistingFile.getKey()).getAbsoluteFile(), s3.getUrl(path.getHost(), prefix + preexistingFile.getKey()));
+          }
+        }
         for (File subfile : file.listFiles()) {
-          map.putAll(upload(s3, path.resolve(file.getName()), subfile));
+          map.putAll(upload(s3, path.resolve(file.getName() + "/"), subfile));
         }
       }
       return map;
